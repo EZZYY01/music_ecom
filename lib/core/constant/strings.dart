787b4dac7a0e00158f68ca0eefad314fa2e7bfa2
Pratik Kc <pratikkc@Pratiks-MacBook-Air.local>
2025-03-05 19:19:50 +0545

    COMPLETING

---
+++ lib/core/constant/strings.dart
@@ -0,0 +1,13 @@
+// App
+const String appTitle = 'SoundSage';
+
+// Networking and APIs
+// const String baseUrl = 'https://e-commerce-mock-api-webservice.onrender.com';
+const String baseUrl = 'http://localhost:4000';
+const String baseUrl1 =  'http://localhost:5002/api';
+const String defaultApiKey = '';
+const String defaultSources = '';
+
+// Storage and Databases
+const String articlesTableName = '';
+const String databaseName = '';
